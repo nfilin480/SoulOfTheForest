f6715140cd6997df01d3aa89a58417549be15f82
Kalyushin <60959640+Kalyushin@users.noreply.github.com>
2022-05-19 20:56:31 +0300

    alpha 1.0
    Объединение интерфейса и игры, добавление меню паузы, подключение музыки в игре

---
+++ Assets/UI/Scripts/MenuControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+
+
+}
